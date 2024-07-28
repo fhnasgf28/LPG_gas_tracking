a8568822d494be29ecac61f9e974f16f9083b8fb
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-07-28 21:01:57 +0700

    [EDIT] edit model gas_tracking.py and sale_order.py add method create, write & unlink

--- lgp_gas_tracking/models/gas_tracking.py
+++ lgp_gas_tracking/models/gas_tracking.py
@@ -1,10 +1,12 @@
 from odoo import models, fields, api
 
+
 class GasTracking(models.Model):
     _name = 'gas.tracking'
     _description = 'Gas Tracking'
 
-    name = fields.Char(string='Tracking Number', required=True, copy=False, readonly=True, index=True, default=lambda self: self.env['ir.sequence'].next_by_code('gas.tracking'))
+    name = fields.Char(string='Tracking Number', required=True, copy=False, readonly=True, index=True,
+                       default=lambda self: self.env['ir.sequence'].next_by_code('gas.tracking'))
     sale_order_id = fields.Many2one('sale.order', string='Sale Order', required=True)
     product_id = fields.Many2one('product.template', string='Gas Product', related='sale_order_id.product_id')
     status = fields.Selection([
--- lgp_gas_tracking/models/sale_order.py
+++ lgp_gas_tracking/models/sale_order.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError, UserError
 
 
 class SaleOrder(models.Model):
@@ -15,8 +16,7 @@ class SaleOrder(models.Model):
     driver_name = fields.Char(string="Driver Name", related="vehicle_id.driver_id.name", readonly=True)
     gas_tracking_ids = fields.One2many('gas.tracking', 'sale_order_id', string='Gas Tracking Records')
     product_id = fields.Many2one('product.template', string='Gas Product')
-    nama_toko= fields.Char(related="partner_id.nama_toko", string='Nama Toko')
-
+    nama_toko = fields.Char(related="partner_id.nama_toko", string='Nama Toko')
 
     @api.model
     def create(self, vals):
@@ -28,3 +28,43 @@ class SaleOrder(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code(sequence_code) or _('New')
         res = super(SaleOrder, self).create(vals)
         return res
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.constrains('product_id', 'product_uom_qty')
+    def _check_available_stock(self):
+        for line in self:
+            config = self.env['gas.lpg.config'].search([('product_id', '=', line.product_id.id)], limit=1)
+            if config:
+                if line.product_uom_qty > config.available_stock:
+                    raise ValidationError(
+                        f"The Quantity ordered for {line.product_id.name} exceeds the available stock of {config.available_stock}")
+
+    @api.model
+    def create(self, vals):
+        record = super(SaleOrderLine, self).create(vals)
+        self._update_available_stock(record.product_id.id, - record.product_uom_qty)
+        return record
+
+    def write(self, vals):
+        for record in self:
+            if "product_uom_qty" in vals:
+                quantity_diff = vals["product_uom_qty"] - record.product_uom_qty
+                self._update_available_stock(record.product_id.id, - quantity_diff)
+        return super(SaleOrderLine, self).write(vals)
+
+    def unlink(self):
+        for record in self:
+            self._update_available_stock(record.product_id.id, record.product_uom_qty)
+        return super(SaleOrderLine, self).unlink()
+
+    def _update_available_stock(self, product_id, quantity_change):
+        config = self.env["gas.lpg.config"].search([('product_id', '=', product_id)], limit=1)
+        if config:
+            new_stock = config.available_stock + quantity_change
+            if new_stock < 0:
+                raise UserError(
+                    f"Stock for {config.product_id.name} is insufficient. Available stock: {config.available_stock}.")
+            config.sudo().write({'available_stock': new_stock})
