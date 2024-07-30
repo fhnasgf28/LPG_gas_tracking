63e88a2fff3db2c58f6076dc61024eef0aef9be8
fhnasgf <farhan.assegaf19019@student.unsika.ac.id>
2024-07-30 17:13:20 +0700

    [EDIT] edit model product.py

--- lgp_gas_tracking/models/product.py
+++ lgp_gas_tracking/models/product.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import random
+import string
+
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
@@ -16,3 +19,22 @@ class LpgProduct(models.Model):
     product_config_id = fields.Many2one('gas.lpg.config', string='Product Config Id')
     # stock_available = fields.Selection(related='product_config_id.status', string='Stock Available')
 
+
+    @api.model
+    def create(self, vals):
+        if 'default_code' not in vals or not vals['default_code']:
+            vals['default_code'] = self.generate_unique_code()
+        product = super(LpgProduct, self).create(vals)
+        product.product_variant_ids.write({'default_code': product.default_code})
+        return product
+
+    @api.model
+    def generate_unique_code(self):
+        while True:
+            code = ''.join(random.choices(string.digits, k=8))
+            if not self.env['product.product'].search([('default_code', '=', code)]):
+                return code
+
+    _sql_constraints = [
+        ('default_code_unique', 'unique(default_code)', 'The default code must be unique for each product!')
+    ]
